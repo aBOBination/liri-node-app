574476bdbfb9c1746a4b386894bcf9bcb60e4111
Robert Marks <rmarks@nav.com>
2020-04-27 08:50:33 -0600

    spotify done

--- liri.js
+++ liri.js
@@ -4,44 +4,49 @@ var Spotify = require('node-spotify-api');
 var axios = require('axios');
 var spotify = new Spotify(keys.spotify);
 
-var argCommand = process.argv[2]
-var argSearch = process.argv[3]
-
-
+var argCommand = process.argv[2];
+var argSearch = process.argv[3];
 
 function spotifyThis() {
     spotify.search({ type: 'track', query: argSearch }, function (err, data) {
         if (err) {
             return console.log('Error occurred: ' + err);
         }
-
-        console.log(data)
+        const items = data.tracks.items
+        console.log('---------------------------------------------------------')
+        items.forEach(res => {
+            console.log(res.artists[0].name)
+            console.log(res.name)
+            console.log(res.external_urls.spotify)
+            console.log(res.album.name)
+            console.log('---------------------------------------------------------')
+        });
     });
 }
 
 function concertThis() {
-    console.log('stuff')
+    console.log('stuff');
 }
 
 function movieThis() {
-    console.log('stuff')
+    console.log('stuff');
 }
 
 function doWhatItSays() {
-    console.log('stuff')
+    console.log('stuff');
 }
 
 switch (argCommand) {
     case 'concert-this':
-        concertThis()
+        concertThis();
         break;
     case 'spotify-this-song':
-        spotifyThis()
+        spotifyThis();
         break;
     case 'movie-this':
-        movieThis()
+        movieThis();
         break;
     case 'do-what-it-says':
-        doWhatItSays()
+        doWhatItSays();
         break;
 }
