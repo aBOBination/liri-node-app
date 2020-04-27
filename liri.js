f6afb91ecfe63170492fc3b06d87d8fd223b3994
Robert Marks <rmarks@nav.com>
2020-04-26 23:34:30 -0600

    spotify added

--- liri.js
+++ liri.js
@@ -1,1 +1,47 @@
-var axios = require('axios')
+require('dotenv').config();
+var keys = require('./keys.js');
+var Spotify = require('node-spotify-api');
+var axios = require('axios');
+var spotify = new Spotify(keys.spotify);
+
+var argCommand = process.argv[2]
+var argSearch = process.argv[3]
+
+
+
+function spotifyThis() {
+    spotify.search({ type: 'track', query: argSearch }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+
+        console.log(data)
+    });
+}
+
+function concertThis() {
+    console.log('stuff')
+}
+
+function movieThis() {
+    console.log('stuff')
+}
+
+function doWhatItSays() {
+    console.log('stuff')
+}
+
+switch (argCommand) {
+    case 'concert-this':
+        concertThis()
+        break;
+    case 'spotify-this-song':
+        spotifyThis()
+        break;
+    case 'movie-this':
+        movieThis()
+        break;
+    case 'do-what-it-says':
+        doWhatItSays()
+        break;
+}
