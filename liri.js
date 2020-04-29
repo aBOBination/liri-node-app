35420ef73ad5624634da794495f13e6cfe29f061
Robert Marks <robert.marks54@gmail.com>
2020-04-28 21:30:20 -0600

    random text start

--- liri.js
+++ liri.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+var fs = require('fs');
 var keys = require('./keys.js');
 var Spotify = require('node-spotify-api');
 var axios = require('axios');
@@ -8,14 +9,14 @@ var argSearch = process.argv[3];
 var figlet = require('figlet');
 
 function sexyText(run) {
-    figlet(argSearch, function(err, data) {
+    figlet(argSearch, function (err, data) {
         if (err) {
             console.log('Something went wrong...');
             console.dir(err);
             return;
         }
-        console.log(data)
-        run
+        console.log(data);
+        run;
     });
 }
 
@@ -45,10 +46,10 @@ function concertThis() {
             // handle success
             data = response.data;
             console.log('---------------------------------------------------------');
-            data.forEach(item => {
-                console.log(item.datetime) // "MM/DD/YYYY"
-                console.log(item.venue.name)
-                console.log(item.venue.location)
+            data.forEach((item) => {
+                console.log(item.datetime); // "MM/DD/YYYY"
+                console.log(item.venue.name);
+                console.log(item.venue.location);
                 console.log('---------------------------------------------------------');
             });
         })
@@ -62,11 +63,54 @@ function concertThis() {
 }
 
 function movieThis() {
-    console.log('stuff');
+    var omdb_key = keys.omdb.key;
+    var url = 'http://www.omdbapi.com/?apikey=' + omdb_key + '&t=' + argSearch;
+
+    axios
+        .get(url)
+        .then(function (response) {
+            // handle success
+            data = response.data;
+            console.log('---------------------------------------------------------');
+            console.log('Title                  : ' + data.Title); // "MM/DD/YYYY"
+            console.log('Year                   : ' + data.Year);
+            console.log('IMDB Rating            : ' + data.imdbRating);
+            data.Ratings.forEach((rating) => {
+                if (rating.Source === 'Rotten Tomatoes') {
+                    console.log('Rotten Tomatoes Rating : ' + rating.Value);
+                }
+            });
+            console.log('Country                : ' + data.Country);
+            console.log('Language               : ' + data.Language);
+            console.log('Plot                   : ' + data.Plot);
+            console.log('Actors                 : ' + data.Actors);
+            console.log('---------------------------------------------------------');
+        })
+        .catch(function (error) {
+            // handle error
+            console.log(error);
+        })
+        .finally(function () {
+            // always executed
+        });
 }
 
 function doWhatItSays() {
-    console.log('stuff');
+    fs.readFile('random.txt', 'utf8', function (error, data) {
+        // If the code experiences any errors it will log the error to the console.
+        if (error) {
+            return console.log(error);
+        }
+
+        // We will then print the contents of data
+        console.log(data);
+
+        // Then split it by commas (to make it more readable)
+        var dataArr = data.split(',');
+
+        // We will then re-display the content as an array for later use.
+        console.log(dataArr);
+    });
 }
 
 switch (argCommand) {
@@ -74,7 +118,7 @@ switch (argCommand) {
         sexyText(concertThis());
         break;
     case 'spotify-this-song':
-        sexyText(spotifyThis())
+        sexyText(spotifyThis());
         break;
     case 'movie-this':
         movieThis();
